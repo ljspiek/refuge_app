fa8dea45bced9e67a692485d32cbb2317077bad0
Laura Spieker <ljspiek@gmail.com>
2019-11-19 06:50:27 -0600

    updated geocodor.js - added refuge api fetch & loop - work in progress

--- geocoder.js
+++ geocoder.js
@@ -1,3 +1,5 @@
+const url = "https://www.refugerestrooms.org/api/v1/restrooms?";
+//creating default with center location in Washington, DC
 function initMap() {
     var map = new google.maps.Map(document.getElementById('map'), {
       zoom: 16,
@@ -8,8 +10,25 @@ function initMap() {
     document.getElementById('submit').addEventListener('click', function() {
       geocodeAddress(geocoder, map);
     });
+
+    getRefugeLocations (function (data) {
+        // let locations = data.name;
+        let name, latLng;
+
+        for (i in data) {
+            name = data[i].name;
+            latLng = new google.maps.LatLng(data[i].latitude, data[i].longitude);
+
+            let marker = new google.maps.Marker({
+                position: latLng,
+                map: map,
+                title: data[i].name,
+            });
+        }
+    })
   }
   
+  //allows user to enter address and center map on location
   function geocodeAddress(geocoder, resultsMap) {
     var address = document.getElementById('address').value;
     geocoder.geocode({'address': address}, function(results, status) {
@@ -24,4 +43,13 @@ function initMap() {
       }
     });
   }
+
+  function getRefugeLocations (callback) {
+      fetch(url)
+        .then(response => response.json())
+        .then(responseJson => displayResults(responseJson))
+        .catch(error => alert('Something went wrong. Try again later.'));
+        
+  }
+
 
