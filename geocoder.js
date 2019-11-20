aad8cdee700adf80f70ce058bc22cc6463fa4a9b
Omar Muhanna <omarmuhomar@hotmail.com>
2019-11-19 18:05:18 -0600

    changed geocoder

--- geocoder.js
+++ geocoder.js
@@ -1,4 +1,5 @@
-const url = "https://www.refugerestrooms.org/api/v1/restrooms?";
+const rootUrl = 
+"https://www.refugerestrooms.org/api/v1/restrooms/by_location?page=1&per_page=10&offset=0";
 //creating default with center location in Washington, DC
 function initMap() {
     var map = new google.maps.Map(document.getElementById('map'), {
@@ -11,27 +12,16 @@ function initMap() {
       geocodeAddress(geocoder, map);
     });
 
-    getRefugeLocations (function (data) {
-        // let locations = data.name;
-        let name, latLng;
-
-        for (i in data) {
-            name = data[i].name;
-            latLng = new google.maps.LatLng(data[i].latitude, data[i].longitude);
-
-            let marker = new google.maps.Marker({
-                position: latLng,
-                map: map,
-                title: data[i].name,
-            });
-        }
-    })
-  }
-  
+   
   //allows user to enter address and center map on location
   function geocodeAddress(geocoder, resultsMap) {
     var address = document.getElementById('address').value;
-    geocoder.geocode({'address': address}, function(results, status) {
+    geocoder.geocode({ address }, function(results, status) {
+      const lat = results[0].geometry.location.lat();
+      const lng = results[0].geometry.location.lng();
+      fetch(`${rootUrl}&lat=${lat}&lng=${lng}`)
+         .then(res => res.json()
+         .then(data => console.log(data)));
       if (status === 'OK') {
         resultsMap.setCenter(results[0].geometry.location);
         var marker = new google.maps.Marker({
@@ -43,13 +33,4 @@ function initMap() {
       }
     });
   }
-
-  function getRefugeLocations (callback) {
-      fetch(url)
-        .then(response => response.json())
-        .then(responseJson => displayResults(responseJson))
-        .catch(error => alert('Something went wrong. Try again later.'));
-        
-  }
-
-
+}
