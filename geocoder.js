94329d507563370b90a142eb2be1c19c5b571ec1
Laura Spieker <ljspiek@gmail.com>
2019-11-19 20:07:19 -0600

    MVP finishing touches

--- geocoder.js
+++ geocoder.js
@@ -1,36 +1,72 @@
 const rootUrl = 
-"https://www.refugerestrooms.org/api/v1/restrooms/by_location?page=1&per_page=10&offset=0";
+"https://www.refugerestrooms.org/api/v1/restrooms/by_location?page=1&per_page=100&offset=0";
 //creating default with center location in Washington, DC
 function initMap() {
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 16,
-      center: {lat: 38.9072, lng: -77.0369}
-    });
-    var geocoder = new google.maps.Geocoder();
-  
-    document.getElementById('submit').addEventListener('click', function() {
-      geocodeAddress(geocoder, map);
-    });
+  let myLatLng = {lat: 38.9071923, lng: -77.0368707};
 
-   
-  //allows user to enter address and center map on location
-  function geocodeAddress(geocoder, resultsMap) {
-    var address = document.getElementById('address').value;
-    geocoder.geocode({ address }, function(results, status) {
-      const lat = results[0].geometry.location.lat();
-      const lng = results[0].geometry.location.lng();
-      fetch(`${rootUrl}&lat=${lat}&lng=${lng}`)
-         .then(res => res.json()
-         .then(data => console.log(data)));
-      if (status === 'OK') {
-        resultsMap.setCenter(results[0].geometry.location);
-        var marker = new google.maps.Marker({
-          map: resultsMap,
-          position: results[0].geometry.location
-        });
-      } else {
-        alert('Geocode was not successful for the following reason: ' + status);
-      }
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 16,
+    center: {lat: 38.9072, lng: -77.0369}
+  });
+  var geocoder = new google.maps.Geocoder();
+
+  document.getElementById('submit').addEventListener('click', function() {
+    geocodeAddress(geocoder, map);
+    
+    let marker = new google.maps.Marker({
+      position: myLatLng,
+      map: map,
+      title: "test",
+      })
+  });
+}
+
+function placeMarkers(data, map) {
+  for(let i = 0; i < data.length; i++) {
+    let name = data[i].name;
+    let markerLatLng = {lat: data[i].latitude, lng: data[i].longitude}
+    // console.log(name);
+    let directions = data[i].directions;
+    let comments = data[i].comment;
+
+    new google.maps.Marker({
+      position: markerLatLng,
+      map: map,
+      title: name,
+      directions: directions,
+      comments: comments
     });
   }
 }
+   
+  //allows user to enter address and center map on location
+function geocodeAddress(geocoder, resultsMap) {
+  var address = document.getElementById('address').value;
+  geocoder.geocode({ address }, function(results, status) {
+    const lat = results[0].geometry.location.lat();
+    const lng = results[0].geometry.location.lng();
+    // latLng = {`lat:${'lat'}, lng: ${'lng'} `}
+    fetch(`${rootUrl}&lat=${lat}&lng=${lng}`)
+        .then(res => res.json())
+        .then(data => {
+        console.log(data);
+        placeMarkers(data, resultsMap);
+        resultsMap.setCenter(results[0].geometry.location);
+          var marker = new google.maps.Marker({
+            map: resultsMap,
+            position: results[0].geometry.location
+          });
+        } );
+    
+      //if results OK, run function to place markers for that area
+      // placeMarkers(data);
+          
+  });
+}
+
+
+
+// let marker = new google.maps.Marker({
+//   position: myLatLng,
+//   map: map,
+// })
